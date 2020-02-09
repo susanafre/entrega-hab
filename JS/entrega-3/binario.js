25f4e791ed9682edbf5839be560311d2dcddcbd7
susana <sfraga2009@gmail.com>
2020-02-09 13:24:56 +0100

    entrega 3

--- JS/entrega-3/binario.js
+++ JS/entrega-3/binario.js
@@ -1,7 +1,11 @@
 'use strict';
 
-let decimal = +prompt('Dime un número');
+function binaryConverter() {
+  let decimal = +prompt('Dime un número');
 
-let binario = decimal.toString(2);
+  let binario = decimal.toString(2);
 
-console.log(binario);
+  console.log(binario);
+}
+
+binaryConverter();
