f54276ae076e3084f08b5dc1cc7b1f405c8a1211
susanafre <59896995+susanafre@users.noreply.github.com>
2020-06-07 11:57:02 +0200

    Ejercicio hackamusic + cambios en rickmortyapi

---
+++ FRONT/hackamusic/src/components/ArtistCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <!-- CREAMOS V-FOR PARA LAS FICHAS DE LOS ARTISTAS -->
+    <div class="card">
+      <h1>⭐ TOP ARTISTS ⭐</h1>
+      <ul>
+        <li v-for="artist in artists" :key="artist.id">
+          <h1>
+            <p>NAME: {{ artist.name }}</p>
+          </h1>
+          <h2>
+            <p>OYENTES: {{ artist.listeners }}</p>
+          </h2>
+
+          <p>
+            <img :src="artist.image[2]['#text']" />
+          </p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+/* INDICAMOS LAS PROPS */
+export default {
+  name: "ArtistCard",
+  props: {
+    artists: Array,
+  },
+};
+</script>
+
+<!-- ESTILOS -->
+<style scoped>
+@font-face {
+  font-family: "Love Ya Like A Sister";
+  src: url("../assets/LoveYaLikeASister-Regular.ttf");
+}
+.card {
+  font-family: "Love Ya Like A Sister";
+  text-align: center;
+  background: linear-gradient(#ffff66, #e66465, #3385ff, #33ff33, #ff33cc);
+  padding-top: 2rem;
+  padding-bottom: 1rem;
+}
+.card > h1 {
+  background-color: rgb(51, 51, 53);
+  padding: 1rem;
+  color: #ffff66;
+}
+li {
+  list-style: none;
+  border: 3px dotted black;
+  height: 400px;
+  width: 450px;
+
+  display: inline-block;
+  margin: 1rem;
+  background-color: lightgoldenrodyellow;
+  opacity: 0.8;
+}
+p {
+  font-weight: bold;
+}
+</style>
+
+/* 1. Se crea la estructura básica 2 .Se añade el div dentro de template, scoped
+y añadimos el name 3. Creamos el V-for para devolver los datos de la API 4.
+Añadimos los estilos */
---
+++ FRONT/hackamusic/src/views/Error.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- USAMOS VUE-HEADFUL PARA EL NOMBRE DE LAS PÁGINAS -->
+    <vue-headful title="Error Page" description="Error Page" />
+    <!-- CREAMOS LA PÁGINA ERROR -->
+    <div class="error">
+      <h1>ERROR 404</h1>
+      <p>PAGE NOT FOUND!!</p>
+      <p>😱</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error",
+};
+</script>
+
+<style scoped>
+/* ESTILOS */
+@font-face {
+  font-family: "Love Ya Like A Sister";
+  src: url("../assets/LoveYaLikeASister-Regular.ttf");
+}
+div {
+  font-family: "Love Ya Like A Sister";
+  text-align: center;
+  background: linear-gradient(#ffff66, #e66465, #33ff33);
+  width: 100%;
+  height: 900px;
+  display: table;
+}
+.error {
+  display: table-cell;
+  vertical-align: middle;
+}
+h1 {
+  font-size: 150px;
+  -webkit-text-stroke: 2px black;
+  color: transparent;
+}
+p {
+  font-weight: bold;
+  font-size: 80px;
+  -webkit-text-stroke: 2px black;
+  color: transparent;
+}
+</style>
