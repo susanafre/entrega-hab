e53c7c2ad8b5164d7a04f67c634269663b405ad4
susana <sfraga2009@gmail.com>
2020-06-07 11:44:37 +0200

    ejercicio hackamusic

---
+++ FRONT/hackamusic/src/components/MenuTops.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div id="nav">
+      <!-- ENLACE A LAS PÁGINAS SOLICITADAS MEDIANTE NAME -->
+      <router-link :to="{ name: 'Toptracks' }">Top Tracks</router-link> ⭐
+
+      <router-link :to="{ name: 'Topartists' }">Top Artists</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuTops",
+};
+</script>
+
+<style scoped>
+/* ESTILOS */
+@font-face {
+  font-family: "Love Ya Like A Sister";
+  src: url("../assets/LoveYaLikeASister-Regular.ttf");
+}
+
+div {
+  background-color: rgb(51, 51, 53);
+  text-align: center;
+  margin: 0;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #ffff66;
+  font-weight: bold;
+  text-decoration: none;
+  font-size: 30px;
+  font-family: "Love Ya Like A Sister";
+}
+
+#nav a.router-link-exact-active {
+  color: #e66465;
+}
+</style>
+
+/* 1. Se crea la estructura básica 2 .Se añade el div dentro de template, scoped
+y añadimos el name 3. Se añaden los links 4. Añadimos estilos */
---
+++ FRONT/hackamusic/src/views/Home.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <!-- USAMOS VUE-HEADFUL PARA EL NOMBRE DE LAS PÁGINAS -->
+
+    <vue-headful title="Home Page" description="Home page of the musci platform" />
+
+    <!-- AÑADIMOS LOS COMPONENTES AL HTML -->
+
+    <!-- MENÚ -->
+
+    <MenuCustom></MenuCustom>
+
+    <div class="introduction">
+      <h1>Bienvenid@ a HACK A MUSIC!</h1>
+
+      <h2>Aquí podrás encontrar a los artistas más TOP 👇</h2>
+      <!-- añadimos el componente del botón para subscribirse -->
+      <ButtonCustom v-on:subscribeButton="subscribeButton()"></ButtonCustom>
+    </div>
+
+    <!-- MENUTOPS -->
+
+    <MenuTops></MenuTops>
+
+    <!-- TAGSCARD -->
+
+    <TagsCard :tags="tags"></TagsCard>
+
+    <!-- FOOTER -->
+
+    <FooterCustom></FooterCustom>
+  </div>
+</template>
+
+<script>
+/* IMPORTAMOS LOS COMPONENTES */
+
+import api from "@/api/index.js";
+import TagsCard from "@/components/TagsCard.vue";
+import MenuCustom from "@/components/MenuCustom.vue";
+import FooterCustom from "@/components/FooterCustom.vue";
+import MenuTops from "@/components/MenuTops.vue";
+import ButtonCustom from "@/components/ButtonCustom.vue";
+//Llamamos a SWAL
+import Swal from "sweetalert2";
+/* AÑADIMOS LOS COMPONENTES */
+
+export default {
+  name: "Home",
+  components: {
+    TagsCard,
+    MenuCustom,
+    FooterCustom,
+    MenuTops,
+    ButtonCustom
+  },
+  data() {
+    return {
+      tags: []
+    };
+  },
+  created() {
+    api.getTopTags().then(response => (this.tags = response.data.tags.tag));
+  },
+  methods: {
+    subscribeButton() {
+      Swal.fire({
+        title: "Subscríbete a nuestra newsletter! 👇 ",
+        input: "text",
+        inputAttributes: {
+          autocapitalize: "off"
+        },
+        showCancelButton: true,
+        confirmButtonText: "Subscribirme!",
+        showLoaderOnConfirm: true,
+        preConfirm: login => {
+          return fetch(`//api.github.com/users/${login}`)
+            .then(response => {
+              if (!response.ok) {
+                throw new Error(response.statusText);
+              }
+              return response.json();
+            })
+            .catch(error => {
+              Swal.showValidationMessage(`Request failed: ${error}`);
+            });
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+      }).then(result => {
+        if (result.value) {
+          Swal.fire({
+            title: `${result.value.login}'s avatar`,
+            imageUrl: result.value.avatar_url
+          });
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* ESTILOS */
+@font-face {
+  font-family: "Love Ya Like A Sister";
+  src: url("../assets/LoveYaLikeASister-Regular.ttf");
+}
+
+.introduction {
+  background: linear-gradient(#ffff66, #e66465);
+  text-align: center;
+  padding: 3rem;
+  font-family: "Love Ya Like A Sister";
+  margin: 0;
+}
+</style>
