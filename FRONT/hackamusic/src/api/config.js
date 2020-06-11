2d4750641d76220c59e724069ac69d8a4a827c87
susana <sfraga2009@gmail.com>
2020-06-11 17:38:18 +0200

    cambios en hackamusic

--- FRONT/hackamusic/src/api/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  appName: "hackamusic",
-  apiKey: "1282725c378508d337250849afbb3e12",
-  secret: "7544b0eab0972700a13425b1a970c659",
-  registeredTo: "susanafre",
-};
-
-/* Nos registramos en la página de lastfm y especificamos los datos de autenticación de la API para poder trabajar con ella  */
